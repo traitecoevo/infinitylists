93e81ca51defbbfdc31525625401b77b5f361afa
wcornwell <wcornwell@gmail.com>
2023-08-07 15:03:41 +1000

    now for duck river instead of nebraska

--- app.R
+++ app.R
@@ -8,23 +8,19 @@ library(dplyr)
 # This would typically be read from an external file
 # observations <- read.csv("observations.csv")
 observations <- data.frame(
-  taxa = c("PlantA", "PlantB", "PlantA", "PlantC", "PlantD", "PlantE", "PlantF"),
+  taxa = c("PlantA", "PlantA", "PlantA", "PlantA", "PlantA", "PlantA", "PlantA"),
   species = c("Species1", "Species2", "Species3", "Species4", "Species5", "Species6", "Species7"),
   voucher_type = c("Voucher1", "Voucher2", "Voucher1", "Voucher2", "Voucher1", "Voucher2", "Voucher1"),
   collection_date = as.Date(c("2020-01-01", "2021-01-01", "2019-01-01", "2020-01-05", "2021-03-01", "2022-01-01", "2022-03-01")),
-  lat = c(40, 42, 41, 43, 43.5, 44, 42.5),
-  long = c(-100, -102, -101, -103, -102.5, -101.5, -102)
+  lat = c(-33.9, -33.86583, -33.86583, -33.86583, -33.865, -33.867, -33.86583),
+  long = c(151.0106, 151.0113, 151.0113, 151.0106, 151.0106, 151.03, 151.01)
 )
 
 # Mockup list of places with their polygons (these would be real polygons in practice)
 places <- list(
-  "PlaceA" = matrix(c(-99, -99, -104, -104, -99,39, 44, 44, 39, 39), ncol=2),
-  "PlaceB" = matrix(c(-101, -101, -105, -105, -101,42, 45, 45, 42, 42), ncol=2)
-)
-  
-#  ,
-#  "PlaceC" = st_geometry(st_read("places/wategora-reserve-survey-area-approximate-boundaries.kml"))
-#)
+ "Duck1" = st_geometry(st_read("places/wategora-reserve-survey-area-approximate-boundaries.kml", crs = 4326)),
+ "Duck2" = st_geometry(st_read("places/wategora-reserve-survey-area-approximate-boundaries.kml", crs = 4326))
+ )
 
 
 
@@ -43,7 +39,7 @@ server <- function(input, output) {
     data <- observations[observations$taxa == input$taxa, ]
     
     # Check if data is within selected place polygon
-    place_polygon <- st_polygon(list(places[[input$place]]))
+    place_polygon <- places[[input$place]]
     points <- st_as_sf(data, coords = c("long", "lat"), crs = 4326)
     dplyr::tibble(data[st_intersects(points, place_polygon, sparse = FALSE)[, 1], ]) %>%
       dplyr::group_by(taxa) %>% dplyr::mutate(n=dplyr::n()) %>% dplyr::arrange(max(collection_date)) %>% dplyr::slice(1)
@@ -57,7 +53,7 @@ server <- function(input, output) {
     leaflet() %>%
       addTiles() %>%
       addMarkers(data = filtered_data(), ~long, ~lat, popup = ~species) %>%
-      addPolygons(lng = places[[input$place]][, 1], lat = places[[input$place]][, 2], color = "red")
+      addPolygons(data = place_polygon, color = "red")
   })
 }
 
