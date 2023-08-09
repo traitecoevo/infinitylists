75ca720a2cb275aec42d15b92fb10494b4b4833f
Will Cornwell <w.cornwell@unsw.edu.au>
2023-08-09 11:59:39 +1000

    making data file smaller

--- ala_processing.R
+++ ala_processing.R
@@ -9,13 +9,11 @@ read_in_and_subset<-function(path){
            coordinateUncertaintyInMeters,basisOfRecord,references)
 }
 
-#test<-read_csv(ala_dl[5])
-
-
 all_ss<-map_df(ala_dl,read_in_and_subset)
-filter(all_ss,references=="https://www.inaturalist.org/observations/88908727")
+#filter(all_ss,references=="https://www.inaturalist.org/observations/88908727")
+#table(all_ss$basisOfRecord)
+#problems<-filter(all_ss[[5]],is.na(year(eventDate)))
 
-table(all_ss$basisOfRecord)
 
 datasets_of_interest<-c("Australia's Virtual Herbarium","iNaturalist observations","iNaturalist research-grade observations")
 all_ss %>%
@@ -24,10 +22,23 @@ all_ss %>%
   filter(basisOfRecord=="PRESERVED_SPECIMEN" | datasetName %in% datasets_of_interest) %>%
   filter(is.na(coordinateUncertaintyInMeters) | coordinateUncertaintyInMeters<=1000) %>%
   filter(!is.na(decimalLatitude) & !is.na(species))-> ala_inat_avh
-write_csv(ala_inat_avh,"ala_nsw_inat_avh.csv")
 
 
-ala_inat_avh %>% filter(basisOfRecord=="PRESERVED_SPECIMEN") -> only_collections
-  
 
-ala<-read_csv("ala_nsw_inat_avh.csv")
+ala_inat_avh$taxa<-stringr::word(ala_inat_avh$species,1,1)
+ala_inat_avh$voucher_type<-case_when(ala_inat_avh$basisOfRecord=="PRESERVED_SPECIMEN" ~ "Collection",
+                            TRUE ~ "Photograph") 
+ala_inat_avh$lat<-ala_inat_avh$decimalLatitude  
+ala_inat_avh$long<-ala_inat_avh$decimalLongitude
+ala_inat_avh$year<-year(ala_inat_avh$eventDate)
+
+#trying to get file as small as possible
+ala_inat_avh %>%
+  select(taxa,species,year,lat,long,voucher_type) %>%
+  na.omit() %>%
+  filter(year>1800) %>%
+  write_csv("ala_nsw_inat_avh.csv")
+
+
+#ala_inat_avh %>% filter(basisOfRecord=="PRESERVED_SPECIMEN") -> only_collections
+#ala<-read_csv("ala_nsw_inat_avh.csv")
