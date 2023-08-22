65bc1669b43df2f0e6da24003f37729d469b3d6f
Fonti Kar <f.kar@unsw.edu.au>
2023-08-22 14:56:59 +1000

    Refined galah workflow and updated data for app

--- R/galah_download.R
+++ R/galah_download.R
@@ -35,11 +35,6 @@ NSW_plants <-
 # Not read into memory
 # NSW_plants <- open_dataset(sources = here("ala_data/NSW_plants_2023-08-21"), format = "parquet") 
 
-# Summary of BoR
-NSW_plants |> 
-  pull(basisOfRecord) |> 
-  tabyl()
-
 ### Narrowing dataset to what we want 
 datasets_of_interest <- c("Australia's Virtual Herbarium","iNaturalist observations","iNaturalist research-grade observations")
 
@@ -49,18 +44,6 @@ NSW_plants_targetrecords  <- NSW_plants |>
          !is.na(eventDate)) |> 
   filter(!str_detect(species,"spec.$")) # Exclude genus level taxon "spec." 
 
-
-# Checking number of rows
-NSW_plants_targetrecords |> nrow()
-
-# Summary of BoR - We can request these types in query 
-# TODO: galah_filter()
-NSW_plants_targetrecords |> 
-    pull(basisOfRecord) |> 
-    tabyl()
-
-
-NSW_plants_targetrecords 
 # Create new voucher variables
 NSW_plants_vouchervars <- NSW_plants_targetrecords |> 
   mutate(voucher_location = case_when(!is.na(references) ~ references,
@@ -69,16 +52,10 @@ NSW_plants_vouchervars <- NSW_plants_targetrecords |>
                                   TRUE ~ "Photograph") 
   )
 
-# Summary of voucher type 
-NSW_plants_vouchervars |> 
-  pull(voucher_type) |> 
-  tabyl()
-
 # Rename and clean names variables
 NSW_plants_renamed <- NSW_plants_vouchervars |> 
   rename(lat = decimalLatitude,
-         long = decimalLongitude) |> 
-  collect() # Here we are finally reading data into memory because the downstream steps require passing to other packages
+         long = decimalLongitude) 
 
 # Reformat date
 NSW_plants_tzdate  <- NSW_plants_renamed |> 
@@ -98,7 +75,7 @@ NSW_plants_native <- left_join(NSW_plants_tzdate,lu) |>
                                                TRUE ~ "unknown") 
   )
 
-# Subset
+# Subset for app
 NSW_plants_cleaned <- NSW_plants_native |>
   select(species:family, collectionDate,
          lat,long,
@@ -108,49 +85,6 @@ NSW_plants_cleaned <- NSW_plants_native |>
           voucher_type, voucher_location, recordedBy,
           collectionDate, family) |> 
 clean_names("title")
-
-
-### Checking
-## Check for every taxon there is collection and photograph
-NSW_plants_cleaned |> 
-  group_by(Species, `Voucher Type`) |> 
-  summarise(n = n()) |> 
-  filter(n == 1)
-
-# Fixing date formats
-NSW_plants_cleaned |> 
-  data.table() |> 
-  filter(`Recorded by` == "Falster, Daniel") |> 
-  select(Species, `Voucher Type`, `Collection Date`) |> 
-  arrange(Species, `Voucher Type`, `Collection Date`)  |> 
-  group_by(Species) |> 
-  summarise(first = first(`Collection Date`)) |> 
-  mutate(date = format(first, "%d-%b-%Y"))
-
-skim(NSW_plants_cleaned)
-
-NSW_plants_cleaned |> View()
-
-## Read in WC's version
-ala <- open_csv_dataset(here("ala_nsw_inat_avh.csv")) |> collect()
-
-skim(ala)
-
-# Check what differs between ala and galah
-setdiff(unique(NSW_plants_cleaned$Species), unique(ala$species)) # Taxa in galah but not in ala
-setdiff(unique(ala$species), unique(NSW_plants_cleaned$Species)) # Taxa in ala but not in galah 
-
-
-
-ala |>
-  mutate(year = year(collectionDate)) |>
-  filter(year >= 1923) |>
-  drop_na(year, species, lat) |>
-  nrow()
-
-ala |> 
-  pull(voucher_type) |> 
-  tabyl()
 
 ### Saving
 write_parquet(NSW_plants_cleaned, paste0("infinity-app/data/NSW_plants_cleaned", Sys.Date(), ".parquet"))
