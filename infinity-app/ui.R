ba30dd86279716b0cbf6a20ae27d2fc608b6b77c
Will Cornwell <w.cornwell@unsw.edu.au>
2023-08-30 16:32:49 +1000

    FAQs

--- infinity-app/ui.R
+++ infinity-app/ui.R
@@ -11,6 +11,9 @@ ui <-
       "An Infinity of Lists: an Interactive Guide to the Australian Biodiversity"
     ),
     add_busy_spinner(spin = "fading-circle", color = "#0dc5c1"),
+    tabsetPanel(
+      # Existing content wrapped in a tabPanel
+      tabPanel("Main", 
     selectInput("ala_path", "Choose a file:", choices = files_in_directory),
     radioButtons(
       "inputType",
@@ -115,8 +118,31 @@ ui <-
     ),
     downloadButton('downloadData', 'Download all obs CSV'),
     tags$br(),
-    textOutput("statsOutput"),
+    div(style = "font-weight: bold; font-size: 24px; margin-top: 20px; margin-bottom: 20px;", textOutput("statsOutput")),
     tags$br(),
     DTOutput("table"),
-    leafletOutput("map", height = 500)
+    leafletOutput("map", height = 500),
+    div(style = "margin-bottom: 50px;") 
+      ),
+    
+    # New FAQ tabPanel
+    tabPanel("FAQ",
+             h2("Frequently Asked Questions"),
+             
+             h4("1. What does this app do?"),
+             p("This app allows users to explore the biodiversity in various areas in Australia."),
+             
+             h4("2. How do I select a location?"),
+             p("You can select a location using one of the three input methods: 'Preloaded Place', 'Upload KML', or 'Choose a place in Australia'."),
+             
+             h4("3. What is a buffer?"),
+             p("A buffer is an additional area around your selected location that you can include in your data query."),
+             
+             h4("4. How do I download the data?"),
+             p("You can download the data by clicking on the 'Download all obs CSV' button."),
+             
+             # Add more questions and answers as needed
+    )
+    )
   )
+
