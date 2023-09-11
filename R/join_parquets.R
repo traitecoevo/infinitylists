f3136497e29ebdb1ecad19ee4d2f2d713fe3a7a8
Fonti Kar <f.kar@unsw.edu.au>
2023-09-11 15:56:51 +1000

    Removed a file we don't need

--- R/join_parquets.R
+++ R/join_parquets.R
@@ -5,30 +5,28 @@ library(arrow)
 library(purrr)
 library(dplyr)
 
-source("R/galah_download.R")
-
-# Shandiya's suggestion: 
-
-# Create a global scheme
-# This is needed because in some years references is a bool() and other years it is a string()
-ALA_schema <- schema(
-    field("recordID", string()),
-    field("species", string()),
-    field("genus", string()),
-    field("family", string()),
-    field("decimalLatitude", double()),
-    field("decimalLongitude", double()),
-    field("coordinateUncertaintyInMeters", double()),
-    field("eventDate", timestamp("us", timezone="UTC")),
-    field("datasetName", string()),
-    field("basisOfRecord", string()),
-    field("references", string()),
-    field("institutionCode", string()),
-    field("recordedBy", string()),
-    field("outlierLayerCount", double()),
-    field("isDuplicateOf", string())
-)
+# # Shandiya's suggestion: 
+# 
+# # Create a global scheme
+# # This is needed because in some years references is a bool() and other years it is a string()
+# infinity_schema <- schema(
+#     field("recordID", string()),
+#     field("species", string()),
+#     field("genus", string()),
+#     field("family", string()),
+#     field("decimalLatitude", double()),
+#     field("decimalLongitude", double()),
+#     field("coordinateUncertaintyInMeters", double()),
+#     field("eventDate", timestamp("us", timezone="UTC")),
+#     field("datasetName", string()),
+#     field("basisOfRecord", string()),
+#     field("references", string()),
+#     field("institutionCode", string()),
+#     field("recordedBy", string()),
+#     field("outlierLayerCount", double()),
+#     field("isDuplicateOf", string())
+# )
 
 # Open all the files with our schema
-x <- open_dataset(sources = "ignore/", schema = plantae_schema) 
-x |> write_dataset(path = "infinity-app/data/Australia-Plantae-2023.parquet", format = "parquet") # That works! 
+x <- open_dataset(sources = "ignore/") 
+x |> write_parquet(sink = paste0("infinity-app/data/Australia-Plantae-", Sys.Date(), ".parquet")) # That works! 
