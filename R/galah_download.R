27a46e60584069c0212d239bc89ed31e8bda1629
Will Cornwell <w.cornwell@unsw.edu.au>
2023-09-21 09:15:01 +1000

    simple testing framework

--- R/galah_download.R
+++ R/galah_download.R
@@ -1,68 +1,91 @@
 #' Download observations for Infinity List
 #'
-#' @param taxa 
-#' @param year_range 
-#' @param save_raw_data 
-#' @param output_dir 
+#' @param taxa
+#' @param year_range
+#' @param save_raw_data
+#' @param output_dir
 #' @export
-download_ala_obs <- function(taxa, 
+download_ala_obs <- function(taxa,
                              year_range = c(1923, 2023),
                              save_raw_data = FALSE,
                              output_dir = system.file(package = "infinitylists", "data/")) {
-  
   # 1. Data retrieval
-  ala_obs <- retrieve_data(taxa, year_range, save_raw_data, output_dir)
+  ala_obs <-
+    retrieve_data(taxa, year_range, save_raw_data, output_dir)
   
   # 2. Filtering and processing
   ala_cleaned <- process_data(ala_obs)
   
   # 3. Add additional columns
-  ala_cleaned_with_add_ons <- get_establishment_status(ala_cleaned, taxa)
+  ala_cleaned_with_add_ons <-
+    get_establishment_status(ala_cleaned, taxa)
   
   # 4. Save processed data
   save_data(ala_cleaned_with_add_ons, taxa, output_dir)
   
- 
+  
 }
 
 
 #' Default ALA query
 #'
-#' @param taxa 
-#' @param years 
-query <- function(taxa, years){
-  galah::galah_call() |> 
-  galah::galah_identify(taxa) |> 
-  galah::galah_filter(
-    spatiallyValid == TRUE, 
-    species != "",
-    decimalLatitude != "",
-    year == years,
-    basisOfRecord == c("HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
-  ) |> 
-  galah::galah_select(
-    recordID, species, genus, family, decimalLatitude, decimalLongitude, 
-    coordinateUncertaintyInMeters, eventDate, datasetName, basisOfRecord, 
-    references, institutionCode, recordedBy, outlierLayerCount, isDuplicateOf,sounds
-  )
+#' @param taxa
+#' @param years
+query <- function(taxa, years) {
+  galah::galah_call() |>
+    galah::galah_identify(taxa) |>
+    galah::galah_filter(
+      spatiallyValid == TRUE,
+      species != "",
+      decimalLatitude != "",
+      year == years,
+      basisOfRecord == c("HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
+    ) |>
+    galah::galah_select(
+      recordID,
+      species,
+      genus,
+      family,
+      decimalLatitude,
+      decimalLongitude,
+      coordinateUncertaintyInMeters,
+      eventDate,
+      datasetName,
+      basisOfRecord,
+      references,
+      institutionCode,
+      recordedBy,
+      outlierLayerCount,
+      isDuplicateOf,
+      sounds
+    )
 }
 
 
-retrieve_data_by_years <- function(taxa, 
+retrieve_data_by_years <- function(taxa,
                                    years,
-                                   save_raw_data = NULL, 
-                                   output_dir =NULL) {
-  
+                                   save_raw_data = NULL,
+                                   output_dir = NULL) {
   # Download data from ALA
-  download <- query(taxa, years) |> 
+  download <- query(taxa, years) |>
     galah::atlas_occurrences()
   
   # Save download (optional)
-  if(save_raw_data)
+  if (save_raw_data)
     arrow::write_parquet(
       download,
-      paste0(output_dir, "ALA-Australia-", taxa, "-", first(years), 
-             "-", last(years), "-",  Sys.Date(), ".parquet")
+      paste0(
+        output_dir,
+        "ALA-Australia-",
+        taxa,
+        "-",
+        first(years),
+        "-",
+        last(years),
+        "-",
+        Sys.Date(),
+        ".parquet"
+      )
     )
   
   return(download)
@@ -70,31 +93,32 @@ retrieve_data_by_years <- function(taxa,
 
 
 
-retrieve_data <- function(taxa, 
+retrieve_data <- function(taxa,
                           year_range = c(1923, 2023),
-                          save_raw_data = FALSE, 
-                          output_dir = NULL){
-  
+                          save_raw_data = FALSE,
+                          output_dir = NULL) {
   # Split years into chunks of 10 year intervals
   years <- seq(year_range[1], year_range[2])
   
   # Determine atlas counts
-  n_obs <- query(taxa, years) |> 
+  n_obs <- query(taxa, years) |>
     galah::atlas_counts()
   
   # If less than 1 mil records
-  if(n_obs < 1000000)
-    output <- retrieve_data_by_years(taxa, years, save_raw_data, output_dir)
+  if (n_obs < 1000000)
+    output <-
+    retrieve_data_by_years(taxa, years, save_raw_data, output_dir)
   else{
-    year_chunks <- split(years, ceiling(seq_along(years)/9))
+    year_chunks <- split(years, ceiling(seq_along(years) / 9))
     
     download <-  purrr::map(year_chunks,
-                            purrr::possibly(~retrieve_data_by_years(taxa, years = .x, save_raw_data, output_dir))
-    )
+                            purrr::possibly(
+                              ~ retrieve_data_by_years(taxa, years = .x, save_raw_data, output_dir)
+                            ))
     
     # Collapse list
     output <- dplyr::bind_rows(download)
-  } 
+  }
   
   return(output)
   
@@ -113,12 +137,20 @@ get_establishment_status <- function(ala_cleaned, taxa = taxa) {
       ala_cleaned |> dplyr::left_join(lookup, by = join_by("Species"))
     
     return(ala_cleaned)
-  } if (taxa %in% c("Cicadoidea","Marsupialia","Odonata","Papilionoidea")) { 
-    ala_cleaned$native_anywhere_in_aus <- "native" 
-    ala_cleaned$native_anywhere_in_aus[ala_cleaned$Species %in% c("Danaus plexippus","Pieris rapae")]<-"introduced"
-  } if (!taxa %in% c("Cicadoidea","Marsupialia","Odonata","Papilionoidea","Plantae")){ 
-    ala_cleaned$native_anywhere_in_aus <-"unknown"
-  } 
+  }
+  if (taxa %in% c("Cicadoidea", "Marsupialia", "Odonata", "Papilionoidea")) {
+    ala_cleaned$native_anywhere_in_aus <- "native"
+    ala_cleaned$native_anywhere_in_aus[ala_cleaned$Species %in% c("Danaus plexippus", "Pieris rapae")] <-
+      "introduced"
+    return(ala_cleaned)
+  }
+  if (!taxa %in% c("Cicadoidea",
+                   "Marsupialia",
+                   "Odonata",
+                   "Papilionoidea",
+                   "Plantae")) {
+    ala_cleaned$native_anywhere_in_aus <- "unknown"
+  }
   return(ala_cleaned)
 }
 
@@ -129,34 +161,44 @@ process_data <- function(data) {
     "iNaturalist research-grade observations"
   )
   
-  data |> 
+  data |>
     dplyr::filter(
-      basisOfRecord == "PRESERVED_SPECIMEN" | datasetName %in% datasets_of_interest,
-      is.na(coordinateUncertaintyInMeters) | coordinateUncertaintyInMeters <= 1000,
-      !is.na(eventDate),
-      !str_detect(species, "spec.$")
+      basisOfRecord == "PRESERVED_SPECIMEN" |
+        datasetName %in% datasets_of_interest,
+      is.na(coordinateUncertaintyInMeters) |
+        coordinateUncertaintyInMeters <= 1000,!is.na(eventDate),!str_detect(species, "spec.$")
     ) |>
     dplyr::mutate(
       voucher_location = dplyr::if_else(!is.na(references), references, institutionCode),
-      voucher_type = dplyr::case_when(basisOfRecord == "PRESERVED_SPECIMEN" ~ "Collection", 
-                               !is.na(sounds) ~ "Audio",
-                               TRUE ~ "Photograph"),
+      voucher_type = dplyr::case_when(
+        basisOfRecord == "PRESERVED_SPECIMEN" ~ "Collection",!is.na(sounds) ~ "Audio",
+        TRUE ~ "Photograph"
+      ),
       lat = decimalLatitude,
       long = decimalLongitude,
       collectionDate = lubridate::ymd_hms(eventDate, tz = "UTC", quiet = TRUE),
-      collectionDate = dplyr::if_else(is.na(collectionDate), lubridate::ymd(eventDate, tz = "UTC", quiet = TRUE), collectionDate)
-    ) |> 
+      collectionDate = dplyr::if_else(
+        is.na(collectionDate),
+        lubridate::ymd(eventDate, tz = "UTC", quiet = TRUE),
+        collectionDate
+      )
+    ) |>
     dplyr::select(
-      species:family, collectionDate, lat, long, voucher_type, voucher_location, recordedBy,recordID
-    ) |> 
+      species:family,
+      collectionDate,
+      lat,
+      long,
+      voucher_type,
+      voucher_location,
+      recordedBy,
+      recordID
+    ) |>
     janitor::clean_names("title")
 }
 
 save_data <- function(data, taxa, output_dir) {
-  arrow::write_parquet(
-    data,
-    paste0(output_dir, "Australia-", taxa, "-", Sys.Date(), ".parquet")
-  )
+  arrow::write_parquet(data,
+                       paste0(output_dir, "Australia-", taxa, "-", Sys.Date(), ".parquet"))
 }
 
 # galah_config(email = Sys.getenv("ALA_EMAIL"),
@@ -170,5 +212,3 @@ save_data <- function(data, taxa, output_dir) {
 #   download_ala_obs(taxa = "Cicadoidea")
 #   download_ala_obs(taxa = "Plantae")
 # })
-
-
