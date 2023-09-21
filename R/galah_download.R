b6f96c7d67a905296bd6891c41e17b1e78eb4310
Will Cornwell <w.cornwell@unsw.edu.au>
2023-09-21 15:15:06 +1000

    updating to taxon and adjusting some documentaion

--- R/galah_download.R
+++ R/galah_download.R
@@ -1,10 +1,10 @@
 #' Download Observations for Infinity List
 #'
-#' This function downloads observation data for the given taxa and year range
+#' This function downloads observation data for the given taxon and year range
 #' from the "Infinity List" source, processes and cleans the data, adds additional
 #' columns (e.g., establishment status), and optionally saves the raw and processed data.
 #'
-#' @param taxon A character vector or string specifying the taxa (e.g., species, genus) 
+#' @param taxon A character vector or string specifying the taxon (e.g., species, genus) 
 #'             for which observations are to be downloaded.
 #' @param year_range A numeric vector of length 2 indicating the start and end years 
 #'                   for data retrieval. Default is from 1923 to 2023.
@@ -17,7 +17,7 @@
 #' 1. Retrieve the data from the "Infinity List" source.
 #' 2. Process and clean the retrieved data to remove any inconsistencies.
 #' 3. Add additional columns to the cleaned data, such as the establishment status 
-#'    for the given taxa.
+#'    for the given taxon.
 #' 4. If `save_raw_data` is `TRUE`, save the processed data to the specified `output_dir`.
 #'
 #' @return This function saves the processed data and returns invisibly. The structure and 
@@ -39,10 +39,10 @@ download_ala_obs <- function(taxon,
   
   # 3. Add additional columns
   ala_cleaned_with_add_ons <-
-    get_establishment_status(ala_cleaned, taxa)
+    get_establishment_status(ala_cleaned, taxon)
   
   # 4. Save processed data
-  save_data(ala_cleaned_with_add_ons, taxa, output_dir)
+  save_data(ala_cleaned_with_add_ons, taxon, output_dir)
   
   
 }
@@ -78,12 +78,12 @@ query <- function(taxon, years){
 
 
 #' @noRd
-retrieve_data_by_years <- function(taxa,
+retrieve_data_by_years <- function(taxon,
                                    years,
                                    save_raw_data = NULL,
                                    output_dir = NULL) {
   # Download data from ALA
-  download <- query(taxa, years) |>
+  download <- query(taxon, years) |>
     galah::atlas_occurrences()
   
   # Save download (optional)
@@ -93,7 +93,7 @@ retrieve_data_by_years <- function(taxa,
       paste0(
         output_dir,
         "ALA-Australia-",
-        taxa,
+        taxon,
         "-",
         first(years),
         "-",
@@ -109,7 +109,7 @@ retrieve_data_by_years <- function(taxa,
 
 
 #' @noRd
-retrieve_data <- function(taxa,
+retrieve_data <- function(taxon,
                           year_range = c(1923, 2023),
                           save_raw_data = FALSE,
                           output_dir = NULL) {
@@ -117,19 +117,19 @@ retrieve_data <- function(taxa,
   years <- seq(year_range[1], year_range[2])
   
   # Determine atlas counts
-  n_obs <- query(taxa, years) |>
+  n_obs <- query(taxon, years) |>
     galah::atlas_counts()
   
   # If less than 1 mil records
   if (n_obs < 1000000)
     output <-
-    retrieve_data_by_years(taxa, years, save_raw_data, output_dir)
+    retrieve_data_by_years(taxon, years, save_raw_data, output_dir)
   else{
     year_chunks <- split(years, ceiling(seq_along(years) / 9))
     
     download <-  purrr::map(year_chunks,
                             purrr::possibly(
-                              ~ retrieve_data_by_years(taxa, years = .x, save_raw_data, output_dir)
+                              ~ retrieve_data_by_years(taxon, years = .x, save_raw_data, output_dir)
                             ))
     
     # Collapse list
@@ -141,8 +141,8 @@ retrieve_data <- function(taxa,
 }
 
 #' @noRd
-get_establishment_status <- function(ala_cleaned, taxa = taxa) {
-  if (taxa == "Plantae") {
+get_establishment_status <- function(ala_cleaned, taxon = taxon) {
+  if (taxon == "Plantae") {
     resources <- APCalign::load_taxonomic_resources()
     lookup <-
       APCalign::native_anywhere_in_australia(ala_cleaned$Species, resources = resources)
@@ -154,13 +154,13 @@ get_establishment_status <- function(ala_cleaned, taxa = taxa) {
     
     return(ala_cleaned)
   }
-  if (taxa %in% c("Cicadoidea", "Marsupialia", "Odonata", "Papilionoidea")) {
+  if (taxon %in% c("Cicadoidea", "Marsupialia", "Odonata", "Papilionoidea")) {
     ala_cleaned$native_anywhere_in_aus <- "native"
     ala_cleaned$native_anywhere_in_aus[ala_cleaned$Species %in% c("Danaus plexippus", "Pieris rapae")] <-
       "introduced"
     return(ala_cleaned)
   }
-  if (!taxa %in% c("Cicadoidea",
+  if (!taxon %in% c("Cicadoidea",
                    "Marsupialia",
                    "Odonata",
                    "Papilionoidea",
@@ -216,9 +216,9 @@ process_data <- function(data) {
 
 
 #' @noRd
-save_data <- function(data, taxa, output_dir) {
+save_data <- function(data, taxon, output_dir) {
   arrow::write_parquet(data,
-                       paste0(output_dir, "Australia-", taxa, "-", Sys.Date(), ".parquet"))
+                       paste0(output_dir, "Australia-", taxon, "-", Sys.Date(), ".parquet"))
 }
 
 # galah_config(email = Sys.getenv("ALA_EMAIL"),
@@ -226,9 +226,9 @@ save_data <- function(data, taxa, output_dir) {
 #
 # requireNamespace("job", quietly = TRUE)
 #job::job(packages = c("purrr", "dplyr", "arrow", "janitor", "galah", "stringr", "lubridate"), {
-#   download_ala_obs(taxa = "Papilionoidea")
-#   download_ala_obs(taxa = "Odonata")
-#   download_ala_obs(taxa = "Marsupialia")
-#   download_ala_obs(taxa = "Cicadoidea")
-#   download_ala_obs(taxa = "Plantae")
+#   download_ala_obs(taxon = "Papilionoidea")
+#   download_ala_obs(taxon = "Odonata")
+#   download_ala_obs(taxon = "Marsupialia")
+#   download_ala_obs(taxon = "Cicadoidea")
+#   download_ala_obs(taxon = "Plantae")
 # })
