41a5cfe62014b50742f9c5e81f049490a80bc64a
Fonti Kar <f.kar@unsw.edu.au>
2024-03-01 09:51:44 +1100

    Small test for load_places

---
+++ tests/testthat/test-spatial-global.R
@@ -0,0 +1,16 @@
+test_that("Loading a place successfully", {
+  places <- list.files(system.file(package = "infinitylists", "extdata", "places"))
+  paths <- file.path(system.file(package = "infinitylists", "extdata", "places", places))
+  
+  kmls <- purrr::map(paths,
+             ~load_place(.x)
+             )
+  
+  expect_visible(purrr::map(paths,
+              ~load_place(.x))
+  )
+  
+  expect_type(kmls[1], "list")
+})
+
+
