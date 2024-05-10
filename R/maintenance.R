1bc9b9697d8cbbe553a93b12aa9cca58b4c5f6c3
Fonti Kar <f.kar@unsw.edu.au>
2024-05-10 12:53:22 +1000

    New release code and dcf for deployment

---
+++ R/maintenance.R
@@ -0,0 +1,10 @@
+#  galah::galah_config(email = "wcornwell@gmail.com")
+# 
+#  requireNamespace("job", quietly = TRUE)
+#  job::job(packages = c("purrr", "dplyr", "arrow", "janitor", "galah", "stringr", "lubridate", "infinitylists"), {
+#     download_ala_obs(taxon = "Papilionoidea")
+#     download_ala_obs(taxon = "Odonata")
+#     download_ala_obs(taxon = "Marsupialia")
+#     download_ala_obs(taxon = "Cicadoidea")
+#     download_ala_obs(taxon = "Plantae")
+#   })
