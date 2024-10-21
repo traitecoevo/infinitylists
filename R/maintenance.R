04883035399973a1109ec5447e6f4f818b767724
Fonti Kar <f.kar@unsw.edu.au>
2024-10-22 08:53:21 +1100

    Updated vignette

--- R/maintenance.R
+++ R/maintenance.R
@@ -1,10 +1,14 @@
-#  galah::galah_config(email = "wcornwell@gmail.com")
-# 
-#  requireNamespace("job", quietly = TRUE)
-#  job::job(packages = c("purrr", "dplyr", "arrow", "janitor", "galah", "stringr", "lubridate", "infinitylists"), {
-#     download_ala_obs(taxon = "Papilionoidea")
-#     download_ala_obs(taxon = "Odonata")
-#     download_ala_obs(taxon = "Marsupialia")
-#     download_ala_obs(taxon = "Cicadoidea")
-#     download_ala_obs(taxon = "Plantae")
-#   })
+ # galah::galah_config(email = "wcornwell@gmail.com")
+ # 
+ # requireNamespace("job", quietly = TRUE)
+ # job::job(packages = c("purrr", "dplyr", "arrow", "janitor", "galah", "stringr", "lubridate", "infinitylists"), {
+ #    download_ala_obs(taxon = "Papilionoidea")
+ #    download_ala_obs(taxon = "Odonata")
+ #    download_ala_obs(taxon = "Marsupialia")
+ #    download_ala_obs(taxon = "Cicadoidea")
+ #    download_ala_obs(taxon = "Plantae")
+ #  })
+ # 
+ # 
+ # download_ala_obs(taxon = "Plantae", year_range = c(1991, 1992))
+ # 
